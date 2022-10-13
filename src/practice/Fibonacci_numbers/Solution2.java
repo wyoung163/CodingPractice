f933bccad3a19e6128d424f526e9b5b63927a4b8
wyoung163 <20201014@sungshin.ac.kr>
2022-10-14 00:55:54 +0900

    practicing lv2. calculating fibonacci numbers in a different way - solving timeout and error

---
+++ src/practice/Fibonacci_numbers/Solution2.java
@@ -0,0 +1,32 @@
+package practice.Fibonacci_numbers;
+
+class Solution2 {
+    public static void main(String[] args) {
+        solution(7);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        answer = Fibonacci_numbers(n);
+        return answer;
+    }
+
+    public static int Fibonacci_numbers(int num){
+        int result = 0;
+        int n1 = 1, n2 = 1;
+        if(num > 2)
+            for(int i = 3; i <= num; i++) {
+                result = (n1 + n2) % 1234567;
+                n1 = n2;
+                n2 = result;
+            }
+        else
+            result = num;
+        return result;
+    }
+}
+
+/*
+    문제점 : n > 2인 수로 주어지는데 처음에 이를 고려하지 못하고 solution에서 % 1234567를 시도하면서 n이 2 미만인 수에 대해서도
+    이 조건이 적용되는 문제를 만남
+ */
