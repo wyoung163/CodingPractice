08e4b8893b62ca616369379bef19fd023582fa33
wyoung163 <20201014@sungshin.ac.kr>
2023-02-11 14:52:53 +0900

    practicing lv2. solution2 of counting mandarin using hashmap and entry

--- src/practice/CountingMandarin/Solution2.java
+++ src/practice/CountingMandarin/Solution2.java
@@ -2,13 +2,9 @@ package practice.CountingMandarin;
 
 import java.util.*;
 
-/*
-    정확도 70.6
- */
-
 class Solution2 {
     public int solution(int k, int[] tangerine) {
-        int answer = 0, sum = 0, num = 0, min = 9999;
+        int answer = 0, sum = 0, num = 0, min = 9999, pt = 0;
         HashMap<Integer, Integer> cnt = new HashMap<Integer, Integer>();
 
         for(int i = 0; i < tangerine.length; i++){
@@ -36,19 +32,23 @@ class Solution2 {
         for(int i = 0; i < key.length; i++){
             sum = 0;
             answer = 0;
-            Loop2 :
-            for(int j = i; j < key.length; j++){
-                if(cnt.get(key[j]) > k){
+            pt = i;
+            while(pt < key.length){
+                if(cnt.get(key[pt]) > k){
                     min = 1;
                     break Loop1;
                 }
-                if(cnt.get(key[j]) + sum <= k){
-                    sum += cnt.get(key[j]);
+
+                if(cnt.get(key[pt]) + sum <= k){
+                    sum += cnt.get(key[pt]);
                     answer++;
                 }
-            }
-            if(sum == k){
-                min = Math.min(min, answer);
+                pt++;
+
+                if(sum == k){
+                    min = Math.min(min, answer);
+                    pt = key.length;
+                }
             }
         }
 
