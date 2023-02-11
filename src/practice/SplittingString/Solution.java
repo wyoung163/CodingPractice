5d4b09880279e9aabdb687eaa46eba47b51c0582
wyoung163 <20201014@sungshin.ac.kr>
2023-02-11 17:32:12 +0900

    practicing lv1. solution of splitting string with same counts of same characters and different characters

---
+++ src/practice/SplittingString/Solution.java
@@ -0,0 +1,27 @@
+package practice.SplittingString;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 1, i = 1, c = 1, r = 0;
+        String x = s.substring(0, 1);
+        
+        while(i < s.length()-1){
+            if((s.substring(i, i+1)).equals(x)){
+                c++;
+            } else {
+                r++;
+            }
+
+            if(c == r){
+                answer++;
+                i++;
+                x = s.substring(i, i+1);
+                c = 1;
+                r = 0;
+            }
+            i++;
+        }
+
+        return answer;
+    }
+}
