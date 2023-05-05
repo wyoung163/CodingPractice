f49f60fcf9f3e04cb9131d3800738cbd14b58e30
wyoung163 <20201014@sungshin.ac.kr>
2023-05-05 16:31:02 +0900

    fix: delete unnecessary array

--- src/practice/DefenceGame/Solution.java
+++ src/practice/DefenceGame/Solution.java
@@ -7,9 +7,7 @@ class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        int[] mujuk = new int[k];
-
+        
         for(int i = 0; i < k; i++){
             pq.add(enemy[i]);
         }
