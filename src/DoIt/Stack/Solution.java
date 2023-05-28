d6099653b210b952a7d1760ecbb1f772c557c214
wyoung163 <choiwy0906@gmail.com>
2023-05-28 21:22:14 +0900

    Problem 011, Stack

---
+++ src/DoIt/Stack/Solution.java
@@ -0,0 +1,32 @@
+package DoIt.Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+        for(int i = 0; i < n; i++){
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int cnt = 1;
+        for(int i : num){
+            while(cnt <= i) {
+                stack.add(cnt++);
+                System.out.println('+');
+            }
+
+            if(stack.peek() == i){
+                stack.pop();
+                System.out.println('-');
+            }
+        }
+    }
+}
