7a1c0454492b6991d31cfacd6d651078021b89bf
wyoung163 <choiwy0906@gmail.com>
2023-05-28 21:39:53 +0900

    docs: add remark

--- src/DoIt/Stack/Solution.java
+++ src/DoIt/Stack/Solution.java
@@ -3,9 +3,12 @@ package DoIt.Stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.Stack;
 
+/*
+    Problem 011
+    스택
+ */
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
